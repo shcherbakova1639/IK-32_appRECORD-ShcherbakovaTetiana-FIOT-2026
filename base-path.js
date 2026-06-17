5c36452a723f88dfcd2dec737b2609f27e55573d
Tania <shcherbakova1639@gmail.com>
2026-06-17 10:14:35 +0300

    commit

--- base-path.js
+++ base-path.js
@@ -1,3 +1,3 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production" ? "/IK-32_appRECORD-ShcherbakovaTetiana-FIOT-2026" : "");
